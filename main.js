ffb111b07cd9a8ed0ce9f1e0cfa3e445322f0654
charlie yin <charlie.t.yin@gmail.com>
2021-04-15 12:40:37 -0500

    with three.js

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ let counter = 0;
 
 document.addEventListener('click', () => {
   counter = counter + 1;
-  document.querySelector(`#line-${counter}`).style.opacity = 1;
   console.log(counter);
+  if (counter === 1) {
+    playAudio('./assets/SNAKE_NOISE_PROCESSED.mp3');
+  }
+  if (counter === 20) {
+    function OpenWin() {
+      window.open('./manifesto/index.html', '_self');
+    }
+    window.onclick = OpenWin();
+  }
+  document.querySelector(`#line-${counter}`).style.opacity = 1;
 });
+
+function playAudio(url) {
+  new Audio(url).play();
+}
--- test.js
+++
@@ -1,6 +0,0 @@
-let counter = 0;
-
-document.addEventListener('click', () => {
-  counter = counter + 1;
-  document.querySelector(`#line-${counter}`).classList.add('fadeIn');
-});
